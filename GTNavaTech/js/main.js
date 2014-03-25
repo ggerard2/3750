95129b586017a58a1e32c8f9723dee7fa6f07e56
Keanna Sion <sion.keanna@gmail.com>
2014-03-25 17:06:25 -0400

    adding map geocoder

---
+++ GTNavaTech/js/main.js
@@ -0,0 +1,24 @@
+﻿var buildingSource = new Array();
+
+$(document).ready(function () {
+    Parse.initialize("FbWhMH1sviYaADqZE81YaAy2hz816cBNr7gwHhvT", "flxbQOKGFIWEs23LhJf7i8SGn0WrEtfQSFPnzO6J");
+
+    var Buildings = Parse.Object.extend("Buildings");
+    var query = new Parse.Query(Buildings);
+    query.limit(250);
+    
+    query.find({
+        success: function (results) {
+            results.forEach(function (building) {
+                buildingSource.push(building.get("name").toLowerCase());
+            });
+
+            bind_source_autocomplete();
+        }
+    });
+});
+
+function bind_source_autocomplete() {
+    $(".input_my_location").autocomplete({ source: buildingSource });
+    $(".input_to_location").autocomplete({ source: buildingSource });
+}
--- GTNavaTech/js/map.js
+++ GTNavaTech/js/map.js
@@ -1,4 +1,12 @@
-﻿function initialize() {
+﻿var curr_position;
+var geocoder;
+var map;
+
+String.prototype.toProperCase = function () {
+    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
+
+function initialize() {
     if (navigator.geolocation) {
         var result = navigator.geolocation.getCurrentPosition(success);
     } else {
@@ -9,13 +17,17 @@
 function success(position) {
     //position.coords.latitude, position.coords.longitude
     //gatech center 33.775753, -84.396291
+    geocoder = new google.maps.Geocoder();
+
+    curr_position = [33.775753, -84.396291];
     var myLatlng = new google.maps.LatLng(33.775753, -84.396291);
     var mapOptions = {
         center: myLatlng,
         disableDefaultUI: true,
         zoom: 17
     };
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
     var marker = new google.maps.Marker({
         position: myLatlng,
@@ -26,4 +38,58 @@ function success(position) {
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
+$(document).ready(function() {
+    Parse.initialize("FbWhMH1sviYaADqZE81YaAy2hz816cBNr7gwHhvT", "flxbQOKGFIWEs23LhJf7i8SGn0WrEtfQSFPnzO6J");
 
+    $("#navigateBtn").on("click", function () {
+        var from_address;
+        var to_address;
+
+        var input_from = $(".input_my_location").val();
+        var input_to = $(".input_to_location").val();
+
+        if (input_from == '' || input_from == undefined) {
+            from_address = curr_position;
+        } else {
+            from_address = getAddressFromSearch(input_from);
+        }
+
+        if (input_to != '' && input_to != undefined) {
+            to_address = getAddressFromSearch(input_to);
+        } else {
+            alert("please enter a location to navigate to");
+        }
+    });
+});
+
+function getAddressFromSearch(to_location_text) {
+    var Buildings = Parse.Object.extend("Buildings");
+    var query = new Parse.Query(Buildings);
+    query.equalTo("name", to_location_text.toProperCase());
+
+    query.first({
+        success: function (result) {
+            result.fetch({
+                success: function (result) {
+                    alert("result: " + result.get("name"));
+                    return codeAddress(result.get("address"));
+                }                
+            });
+        }
+    });
+}
+
+
+function codeAddress(address) {
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}

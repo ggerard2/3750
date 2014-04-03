157b7eca6e3642ef2cb293a2bfafa7dbee527112
Ksion <sion.keanna@gmail.com>
2014-04-03 17:16:26 -0400

    wanna work on a different computer...

--- GTNavaTech/js/navigate.js
+++ GTNavaTech/js/navigate.js
@@ -1,1 +1,33 @@
-﻿
+﻿$(document).ready(function () {
+    $('.transOption').on('click', function () {
+        unselectAllTabOptions();
+        $(this).css("background-color", "rgb(230, 193, 0)");
+        var type = $(this).data("type");
+        showContent(type);
+    });
+});
+
+function unselectAllTabOptions() {
+    $.each($('.transOption'), function (index, element) {
+        $(element).css("background-color", "rgb(0, 1, 32)");
+    });   
+}
+
+function unselectContent() {
+    $.each($('.content'), function (index, element) {
+        $(element).addClass('hide');
+    });
+}
+
+function showContent(type) {
+    if (type == 'walk') {
+        unselectContent();
+        $('.walkContent').removeClass('hide');
+    } else if (type == 'bus') {
+        unselectContent();
+        $('.busContent').removeClass('hide');
+    } else {
+        unselectContent();
+        $('.bikeContent').removeClass('hide');
+    }
+}
